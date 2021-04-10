b753014f7328d8fe3dd412a25349a71e8d089d3f
DivineEntity01 <78000390+DivineEntity01@users.noreply.github.com>
2021-04-10 03:33:24 -0600

    Update AtomosphereMain.lua

--- AtomosphereMain.lua
+++ AtomosphereMain.lua
@@ -49,7 +49,7 @@ function Atomos:NewTab(Name)
 	TabTitle.TextColor3 = Color3.fromRGB(188, 189, 208)
 	TabTitle.TextSize = 25.000
 	if TabCount > 1 then
-		local Move = (game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.05):GetChildren()[TabCount - 1].Position.X.Scale + 0.16)
+		local Move = (game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.05):GetChildren()[TabCount - 1].Position.X.Scale + 0.1513)
 		Tab.Position = UDim2.new(Move, 0, 0.0284167733, 0)
 	end
 
