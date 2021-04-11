fb8f971665e3de7051099d4ad6e83fdc1f1e0e4c
DivineEntity01 <78000390+DivineEntity01@users.noreply.github.com>
2021-04-11 00:34:47 -0600

    Added KeyButton for the first time 0.6b

--- AtomosphereMain.lua
+++ AtomosphereMain.lua
@@ -1,6 +1,7 @@
 local Atomos = {}
 local TabCount = 0
 local TS = game:GetService("TweenService")
+local UserInputService = game:GetService("UserInputService")
 
 if game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.001) then
 	game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.001):Destroy()
@@ -31,7 +32,7 @@ function Atomos:NewTab(Name)
 	Tab.ZIndex = 2
 	Tab.Image = "http://www.roblox.com/asset/?id=6657363591"
 	Tab.BackgroundTransparency = 1
-	
+
 	ButtonListing.Name = "ButtonListing"
 	ButtonListing.Parent = Tab
 	ButtonListing.HorizontalAlignment = Enum.HorizontalAlignment.Center
@@ -60,7 +61,7 @@ function Atomos:NewTab(Name)
 		local Button = Instance.new("TextButton")
 		Tab.BackgroundTransparency = 0 --Make the dark top part visible
 		--str
-		Button.Name = "Button"
+		Button.Name = str or "Button"
 		Button.Parent = Tab
 		Button.BackgroundColor3 = Color3.fromRGB(22, 25, 35)
 		Button.BorderSizePixel = 0
@@ -74,29 +75,149 @@ function Atomos:NewTab(Name)
 		Button.TextWrapped = true
 		--Callback
 		Button.MouseButton1Click:Connect(function()
-			local s, e = pcall(callback)
-			if e then
-			    print(e)
+			local s, f = pcall(callback)
+			if f then
+				print("UserError: ", f)
 			end
 			local Clicked = TS:Create(Button, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
 			local Return = TS:Create(Button, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
 			Clicked:Play()
 			Clicked.Completed:Connect(function(playbackState)
-			wait(0.1)
-			Return:Play()
+				wait(0.1)
+				Return:Play()
 			end)
 		end)
-		--Effect #1a1e2a
+		--Effect
 		Button.MouseEnter:Connect(function()
-		local Hover = TS:Create(Button, TweenInfo.new(0.25, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(26, 30, 42)})	
-		Hover:Play()
+			local Hover = TS:Create(Button, TweenInfo.new(0.25, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(26, 30, 42)})	
+			Hover:Play()
 		end)
 		Button.MouseLeave:Connect(function()
-		local Leeave = TS:Create(Button, TweenInfo.new(0.36, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(22, 25, 35)})	
-		Leeave:Play()
+			local Leeave = TS:Create(Button, TweenInfo.new(0.36, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(22, 25, 35)})	
+			Leeave:Play()
 		end)
+		--
 	end
-	--
+	function ElementsList:KeyButton(str, key, change, callback) --KeyBind Button
+		local KeyButton = Instance.new("TextButton")
+		KeyButton.Name = str or "KeyButton"
+		KeyButton.Parent = Tab
+		KeyButton.BackgroundColor3 = Color3.fromRGB(22, 25, 35)
+		KeyButton.BorderSizePixel = 0
+		KeyButton.Position = UDim2.new(0, 0, 1.00000012, 0)
+		KeyButton.Size = UDim2.new(1, 0, 1, 0)
+		KeyButton.AutoButtonColor = false
+		KeyButton.ZIndex = -4
+		KeyButton.Font = Enum.Font.Highway
+		KeyButton.Text = str or "KeyButton"
+		KeyButton.TextColor3 = Color3.fromRGB(188, 189, 208)
+		KeyButton.TextSize = 20.000
+		KeyButton.TextWrapped = true
+		local KeyBind = Instance.new("TextButton")
+		local UICorner = Instance.new("UICorner")
+		local Detect = false
+		local Changing = false
+		if key then
+		key = key:sub(1,1):upper() .. key:sub(2)
+		if key == "Rightalt" or "Leftalt" then
+			key = string.gsub(tostring(key), "alt", "Alt")
+		elseif key == "Rightshift" or "Leftshift" then
+		key = string.gsub(tostring(key), "shift", "Shift")
+		end
+		end
+		KeyBind.Name = key or ""
+		KeyBind.Parent = KeyButton
+		KeyBind.BackgroundColor3 = Color3.fromRGB(40, 42, 54)
+		KeyBind.BorderSizePixel = 0
+		KeyBind.LayoutOrder = -3
+		KeyBind.Position = UDim2.new(0.659, 0, 0, 0)
+		KeyBind.Size = UDim2.new(0.330563664, 0, 1, 0)
+		KeyButton.AutoButtonColor = false
+		KeyBind.ZIndex = -6
+		KeyBind.Font = Enum.Font.Highway
+		KeyBind.Text = key or ""
+		KeyBind.TextColor3 = Color3.fromRGB(172, 172, 172)
+		KeyBind.TextSize = 12.000
+		KeyBind.TextWrapped = true
+		UICorner.CornerRadius = UDim.new(0.200000003, 0)
+		UICorner.Parent = KeyBind
+		--Callback
+			--KeyBind Change
+		if change == true then
+		KeyBind.MouseButton1Click:Connect(function()
+			local Clicked = TS:Create(KeyBind, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
+			local Return = TS:Create(KeyBind, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
+			KeyBind.Text = "..."
+			Changing = true
+			UserInputService.InputBegan:Connect(function(Input , GPE)
+				if Changing then
+				KeyBind.Text = string.sub(tostring(Input.KeyCode), 14)
+				key = string.sub(tostring(Input.KeyCode), 14)
+				Changing = false
+				end
+			end)
+			Clicked:Play()
+			Clicked.Completed:Connect(function(playbackState)
+				wait(0.1)
+				Return:Play()
+			end)
+			end)
+		end
+			--KeyBind Fire
+		UserInputService.InputBegan:Connect(function(input, gp) -- when player presses a key
+			if input.KeyCode == Enum.KeyCode[key] and not gp and not Changing then
+				local s, f = pcall(callback)
+				if f then
+					print("UserError: ", f)
+				end
+			end
+		end)
+			--Button
+		KeyButton.MouseButton1Click:Connect(function()
+			local s, f = pcall(callback)
+			if f then
+				print("UserError: ", f)
+			end
+			local Clicked = TS:Create(KeyButton, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
+			local Return = TS:Create(KeyButton, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
+			Clicked:Play()
+			Clicked.Completed:Connect(function(playbackState)
+				wait(0.1)
+				Return:Play()
+			end)
+		end)
+		--
+		--Effect
+		KeyButton.MouseEnter:Connect(function()
+			local Hover = TS:Create(KeyButton, TweenInfo.new(0.25, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(26, 30, 42)})	
+			local SlideOut = TS:Create(KeyBind, TweenInfo.new(0.25, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {Position = UDim2.new(1, 0, 0, 0)})
+			SlideOut:Play()
+			Hover:Play()
+		end)
+		KeyButton.MouseLeave:Connect(function()
+			local Leeave = TS:Create(KeyButton, TweenInfo.new(0.36, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(26, 30, 42)})	
+			Leeave:Play()
+			wait(0.05)
+			if not Detect then
+				
+			end
+		end)
+		KeyBind.MouseEnter:Connect(function()
+			Detect = true
+			local Hover = TS:Create(KeyBind, TweenInfo.new(0.25, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(26, 30, 42)})	
+			local SlideOut = TS:Create(KeyBind, TweenInfo.new(0.25, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {Position = UDim2.new(1, 0, 0, 0)})
+			SlideOut:Play()
+			Hover:Play()
+		end)
+		KeyBind.MouseLeave:Connect(function()
+			Detect = false
+			local Leaave = TS:Create(KeyBind, TweenInfo.new(0.25, Enum.EasingStyle.Cubic, Enum.EasingDirection.In), {BackgroundColor3 = Color3.fromRGB(40, 42, 54)})	
+			local SlideIn = TS:Create(KeyBind, TweenInfo.new(0.25, Enum.EasingStyle.Cubic, Enum.EasingDirection.In), {Position = UDim2.new(0.659, 0, 0, 0)})
+			SlideIn:Play()
+			Leaave:Play()
+		end)
+		--
+	end
 	return ElementsList
 end
 return Atomos;
