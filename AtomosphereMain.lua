042f9e73394d1c75c48c418ef3c9fce8ec81b412
DivineEntity01 <78000390+DivineEntity01@users.noreply.github.com>
2021-04-12 09:57:00 -0600

    Lost the track of the versions, 0.94b ig

--- AtomosphereMain.lua
+++ AtomosphereMain.lua
@@ -9,6 +9,7 @@ if game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.001) then
 	game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.001):Destroy()
 end
 --[[
+
 [Index]
 ▾AtomosCore
 	▾Atomosphere Main
@@ -34,6 +35,7 @@ CreateMain()
 --[[AtomosTab› ]]
 function Atomos:NewTab(Name) --[[AtomosTab› ]]
 	local Main = game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.01)
+	local CVD = false
 	TabCount = TabCount + 1
 	-------------------------------------------------------------------------------------------------
 	--Create Tab
@@ -41,6 +43,9 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 	local ButtonListing = Instance.new("UIListLayout")
 	local MaxMinSize = Instance.new("UISizeConstraint")
 	local TabTitle = Instance.new("TextLabel")
+	local Close = Instance.new("ImageButton")
+	local Open = Instance.new("ImageButton")
+	local Corner = Instance.new("UICorner")
 	Tab.Name = Name or "Tab".. TabCount
 	Tab.Parent = Main
 	Tab.BackgroundColor3 = Color3.fromRGB(22, 25, 35)
@@ -48,10 +53,10 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 	Tab.LayoutOrder = 5
 	Tab.Position = UDim2.new(0.0173761956, 0, 0.0284167733, 0)
 	Tab.Size = UDim2.new(0.130321458, 0, 0.033829499, 0)
-	Tab.ZIndex = 8
+	Tab.ZIndex = 20
 	Tab.Image = "http://www.roblox.com/asset/?id=6657363591"
 	Tab.BackgroundTransparency = 1
-
+	
 	ButtonListing.Name = "ButtonListing"
 	ButtonListing.Parent = Tab
 	ButtonListing.HorizontalAlignment = Enum.HorizontalAlignment.Center
@@ -66,19 +71,69 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 	TabTitle.BackgroundTransparency = 1.000
 	TabTitle.Position = UDim2.new(0.13333334, 0, 0, 0)
 	TabTitle.Size = UDim2.new(0.733333349, 0, 1, 0)
-	TabTitle.ZIndex = 9
+	TabTitle.ZIndex = 21
 	TabTitle.Font = Enum.Font.Highway
 	TabTitle.Text = Name or "NewTab".. TabCount
 	TabTitle.TextColor3 = Color3.fromRGB(188, 189, 208)
 	TabTitle.TextSize = 25.000
+	Close.Name = "Close"
+	Close.Parent = TabTitle
+	Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Close.BackgroundTransparency = 1.000
+	Close.Position = UDim2.new(0.954545021, 0, 0, 0)
+	Close.Size = UDim2.new(0.227272734, 0, 1, 0)
+	Close.ZIndex = 23
+	Close.AutoButtonColor = false
+	Close.Image = "http://www.roblox.com/asset/?id=6672522633"
+	Close.ScaleType = Enum.ScaleType.Fit
+	Open.Name = "Open"
+	Open.Parent = TabTitle
+	Open.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Open.BackgroundTransparency = 1.000
+	Open.Position = UDim2.new(0.954545021, 0, 0, 0)
+	Open.Size = UDim2.new(0.227272734, 0, 1, 0)
+	Open.ZIndex = 22
+	Open.AutoButtonColor = false
+	Open.ImageTransparency = 1
+	Open.Image = "http://www.roblox.com/asset/?id=6672528304"
+	Open.ScaleType = Enum.ScaleType.Fit
+	Corner.CornerRadius = UDim.new(0, 0)
+	Corner.Name = "Corner"
+	Corner.Parent = Tab
 	if TabCount > 1 then
 		local Move = (game:GetService("CoreGui"):WaitForChild("Atomosphere", 0.05):GetChildren()[TabCount - 1].Position.X.Scale + 0.1513)
 		Tab.Position = UDim2.new(Move, 0, 0.0284167733, 0)
 	end
+	Close.MouseButton1Click:Connect(function()
+		if not CVD then
+			CVD = true
+			local Contract = TS:Create(ButtonListing, TweenInfo.new(0.2, Enum.EasingStyle.Cubic, Enum.EasingDirection.Out), {Padding = UDim.new(-1,0)})
+			Contract:Play()
+			Contract.Completed:Connect(function()
+				for _,v in pairs(Tab:GetChildren()) do
+					if v:IsA("TextButton") then
+						v.Visible = false
+					end
+				end
+				TS:Create(Corner, TweenInfo.new(0.2, Enum.EasingStyle.Cubic, Enum.EasingDirection.In), {CornerRadius = UDim.new(1, 0)}):Play()
+				local HideMinimize = TS:Create(Close, TweenInfo.new(0.35, Enum.EasingStyle.Sine, Enum.EasingDirection.In), {ImageTransparency = 1})
+				HideMinimize:Play()
+				HideMinimize.Completed:Connect(function()
+					Close.ZIndex = 22
+					local ShowMaximize = TS:Create(Close, TweenInfo.new(0.35, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {ImageTransparency = 0})
+					ShowMaximize:Play()
+					ShowMaximize.Completed:Connect(function()
+						Open.ZIndex = 23
+					end)
+				end)
+			end)
+		end
+	end)
+	
 	local ElementsList = {}
-
 	function ElementsList:Button(str, callback) --[[Normal Button› ]]
 		local Button = Instance.new("TextButton")
+		local Icon = Instance.new("ImageLabel")
 		Tab.BackgroundTransparency = 0 --Make the dark top part visible
 		--str
 		Button.Name = str or "ClickButton"
@@ -88,12 +143,22 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		Button.Position = UDim2.new(0, 0, 1.00000012, 0)
 		Button.Size = UDim2.new(1, 0, 1, 0)
 		Button.AutoButtonColor = false
-		Button.ZIndex = 7
+		Button.ZIndex = 8
 		Button.Font = Enum.Font.Highway
 		Button.Text = str or "NewButton"
 		Button.TextColor3 = Color3.fromRGB(188, 189, 208)
 		Button.TextSize = 20.000
 		Button.TextWrapped = true
+		Icon.Name = "Icon"
+		Icon.Parent = Button
+		Icon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		Icon.BackgroundTransparency = 1.000
+		Icon.Position = UDim2.new(0.833333015, 0, 0.12987946, 0)
+		Icon.Size = UDim2.new(0.159999996, 0, 0.839999974, 0)
+		Icon.Image = "http://www.roblox.com/asset/?id=6671933955"
+		Icon.ZIndex = 9
+		Icon.ImageTransparency = 0.200
+		Icon.ScaleType = Enum.ScaleType.Fit
 		--Callback
 		Button.MouseButton1Click:Connect(function()
 			local s, f = pcall(callback)
@@ -102,9 +167,13 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 			end
 			local Clicked = TS:Create(Button, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
 			local Return = TS:Create(Button, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
+			local IconClicked = TS:Create(Icon, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {ImageTransparency = 0})
+			local IconReturn = TS:Create(Icon, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageTransparency = 0.200})
 			Clicked:Play()
+			IconClicked:Play()
 			Clicked.Completed:Connect(function()
 			wait(0.1)
+			IconReturn:Play()
 			Return:Play()
 			end)
 		end)
@@ -122,6 +191,7 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 	-- ‹
 	function ElementsList:KeyButton(str, key, change, callback) --[[Keybind Button› ]]
 		local KeyButton = Instance.new("TextButton")
+		local Icon = Instance.new("ImageLabel")
 		KeyButton.Name = str or "KeyButton"
 		KeyButton.Parent = Tab
 		KeyButton.BackgroundColor3 = Color3.fromRGB(22, 25, 35)
@@ -129,12 +199,22 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		KeyButton.Position = UDim2.new(0, 0, 1.00000012, 0)
 		KeyButton.Size = UDim2.new(1, 0, 1, 0)
 		KeyButton.AutoButtonColor = false
-		KeyButton.ZIndex = 7
+		KeyButton.ZIndex = 8
 		KeyButton.Font = Enum.Font.Highway
 		KeyButton.Text = str or "KeyButton"
 		KeyButton.TextColor3 = Color3.fromRGB(188, 189, 208)
 		KeyButton.TextSize = 20.000
 		KeyButton.TextWrapped = true
+		Icon.Name = "Icon"
+		Icon.Parent = KeyButton
+		Icon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		Icon.BackgroundTransparency = 1.000
+		Icon.Position = UDim2.new(0.833333015, 0, -0.0800001025, 0)
+		Icon.Size = UDim2.new(0.173333541, 0, 1.12000012, 0)
+		Icon.Image = "http://www.roblox.com/asset/?id=6672097066"
+		Icon.ImageTransparency = 0.200
+		Icon.ZIndex = 9
+		Icon.ScaleType = Enum.ScaleType.Fit
 		local KeyBind = Instance.new("TextButton")
 		local UICorner = Instance.new("UICorner")
 		local Detector = Instance.new("Frame")
@@ -172,7 +252,7 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		--Callback
 			--KeyBind Change
 		if change == true then
-		KeyBind.MouseButton1Click:Connect(function()
+			KeyBind.MouseButton1Click:Connect(function()
 			local Clicked = TS:Create(KeyBind, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
 			local Return = TS:Create(KeyBind, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
 			KeyBind.Text = "[...]"
@@ -200,6 +280,20 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		if key then
 		UserInputService.InputBegan:Connect(function(input, gp) -- when player presses a key
 			if input.KeyCode == Enum.KeyCode[key] and not gp and not Changing then
+			spawn(function()
+				local IconClicked = TS:Create(Icon, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {ImageTransparency = 0})
+				local IconReturn = TS:Create(Icon, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageTransparency = 0.200})
+				local Clicked = TS:Create(KeyButton, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
+				local Return = TS:Create(KeyButton, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
+				Clicked:Play()
+				IconClicked:Play()
+				Clicked.Completed:Connect(function(playbackState)
+				wait(0.1)
+				IconReturn:Play()
+				Return:Play()
+				end)
+			end)
+			
 				local s, f = pcall(callback)
 				if f then
 					print("UserError: ", f)
@@ -213,12 +307,16 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 			if f then
 				print("UserError: ", f)
 			end
+			local IconClicked = TS:Create(Icon, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {ImageTransparency = 0})
+			local IconReturn = TS:Create(Icon, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageTransparency = 0.200})
 			local Clicked = TS:Create(KeyButton, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(240, 238, 249)})
 			local Return = TS:Create(KeyButton, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextColor3 = Color3.fromRGB(188, 189, 208)})
 			Clicked:Play()
+			IconClicked:Play()
 			Clicked.Completed:Connect(function(playbackState)
-				wait(0.1)
-				Return:Play()
+			wait(0.1)
+			IconReturn:Play()
+			Return:Play()
 			end)
 		end)
 		--
@@ -266,6 +364,7 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		local SliderText = Instance.new("TextLabel")
 		local Container = Instance.new("Frame")
 		local RatioConstraint = Instance.new("UIAspectRatioConstraint")
+		local Icon = Instance.new("ImageLabel")
 		Slider.Name = str or "DragSlider"
 		Slider.Parent = Tab
 		Slider.BackgroundColor3 = Color3.fromRGB(22, 25, 35)
@@ -273,7 +372,7 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		Slider.Position = UDim2.new(0, 0, 1.00000012, 0)
 		Slider.Size = UDim2.new(1, 0, 1, 0)
 		Slider.AutoButtonColor = false
-		Slider.ZIndex = 7
+		Slider.ZIndex = 8
 		Slider.Font = Enum.Font.Highway
 		Slider.Text = str or "DragSlider"
 		Slider.TextColor3 = Color3.fromRGB(188, 189, 208)
@@ -326,7 +425,7 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		ValueBG.ClipsDescendants = true
 		ValueBG.Position = UDim2.new(-0.100355983, 0, 1, 0)
 		ValueBG.Size = UDim2.new(1.20038807, 0, 1.00999999, 0)
-		ValueBG.ZIndex = 5
+		ValueBG.ZIndex = 6
 		ValueBG.Image = "http://www.roblox.com/asset/?id=6661850089"
 		ValueBG.ScaleType = Enum.ScaleType.Fit
 		SliderText.Name = "SliderText"
@@ -335,7 +434,7 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		SliderText.BackgroundTransparency = 1.000
 		SliderText.Position = UDim2.new(0, 0, 0.0799999982, 0)
 		SliderText.Size = UDim2.new(1, 0, 0.479999989, 0)
-		SliderText.ZIndex = 6
+		SliderText.ZIndex = 7
 		SliderText.Font = Enum.Font.Highway
 		SliderText.Text = ""
 		SliderText.TextColor3 = Color3.fromRGB(200, 203, 231)
@@ -345,6 +444,16 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 		RatioConstraint.Name = "RatioConstraint"
 		RatioConstraint.Parent = ValueBG
 		RatioConstraint.AspectRatio = 1.426
+		Icon.Name = "Icon"
+		Icon.Parent = Slider
+		Icon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		Icon.BackgroundTransparency = 1.000
+		Icon.Position = UDim2.new(0.833333015, 0, 0.12987946, 0)
+		Icon.Size = UDim2.new(0.159999996, 0, 0.839999974, 0)
+		Icon.Image = "http://www.roblox.com/asset/?id=6672257305"
+		Icon.ZIndex = 9
+		Icon.ImageTransparency = 0.200
+		Icon.ScaleType = Enum.ScaleType.Fit
 		--Effect
 		--Slider
 		Slider.MouseEnter:Connect(function()
@@ -403,8 +512,10 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 				local function Two()
 				local DraggingOn = TS:Create(SliderLine, TweenInfo.new(0.17, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {BackgroundColor3 = Color3.fromRGB(255, 178, 178)})
 				local DraggingOnT = TS:Create(SliderLine, TweenInfo.new(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundTransparency = 0.4})
+				local IconClicked = TS:Create(Icon, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {ImageTransparency = 0})
 				DraggingOn:Play()
 				DraggingOnT:Play()	
+				IconClicked:Play()	
 				end	
 				local function One()
 				local Show = TS:Create(ValueBG, TweenInfo.new(0.23, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {Position = UDim2.new(-0.100355983, 0, 0, 0)})
@@ -424,8 +535,10 @@ function Atomos:NewTab(Name) --[[AtomosTab› ]]
 				local function Two()
 				local DraggingOff = TS:Create(SliderLine, TweenInfo.new(0.36, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {BackgroundColor3 = Color3.fromRGB(255, 20, 25)})
 				local DraggingOffT = TS:Create(SliderLine, TweenInfo.new(0.36, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {BackgroundTransparency = 0})
+				local IconReturn = TS:Create(Icon, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageTransparency = 0.200})	
 				DraggingOff:Play()
 				DraggingOffT:Play()	
+				IconReturn:Play()
 				end
 				local function One()
 				local Hide = TS:Create(ValueBG, TweenInfo.new(0.36, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {Position = UDim2.new(-0.100355983, 0, 1, 0)})
